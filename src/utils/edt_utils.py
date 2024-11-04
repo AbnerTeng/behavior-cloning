88f208b2503b1a56f983cd7c05e8bf830d5e1865
b08201047 <b08201047@ws2.csie.ntu.edu.tw>
2024-11-04 17:42:29 +0800

    fix: server issue

--- src/utils/edt_utils.py
+++ src/utils/edt_utils.py
@@ -1,6 +1,10 @@
 import torch
 
 
-def expectile_loss(diff: torch.Tensor, expectile: float = 0.8) -> torch.Tensor:
+def expectile_loss(diff: torch.Tensor, expectile: float = 0.99) -> torch.Tensor:
+    """
+    diff (torch.Tensor): difference between the target and the imp_loss_return
+    expectile (float): expectile value (default: 0.99)
+    """
     weight = torch.where(diff > 0, expectile, (1 - expectile))
     return weight * (diff ** 2)
