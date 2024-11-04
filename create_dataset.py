3fc8d91aee92742cab94902c48c779c9f7ec9a6b
Yu-Chen, Den <abnerteng16@gmail.com>
2024-11-04 15:23:29 +0800

    dt_code

---
+++ create_dataset.py
@@ -0,0 +1,162 @@
+"""
+Dataset creation module
+"""
+from typing import Tuple, Optional
+
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+
+from .utils.utils import (
+    min_max_norm,
+    count_return_to_go
+)
+
+
+class TradeLogDataset(Dataset):
+    """
+    Dataset of trading logs
+    """
+
+    def __init__(self, *args) -> None:
+        self.data = args
+
+    def __len__(self) -> int:
+        return len(self.data[0])
+
+    def __getitem__(self, idx: int) -> Tuple:
+        return tuple(
+            d[idx] for d in self.data
+        )
+
+
+class EDTTradeLogDataset(Dataset):
+    """
+    Dataset of Elastic Decision Transformer trading logs
+    """
+
+    def __init__(self, *args) -> None:
+        self.data = args
+
+    def __len__(self) -> int:
+        return len(self.data[0])
+
+    def __getitem__(self, idx: int) -> Tuple:
+        return tuple(
+            d[idx] for d in self.data
+        )
+
+
+class DataPreprocess:
+    """
+    Preprocess class
+
+    Args:
+        max_len: int --> window size for the sequence
+        state_size: int --> state size (4 here)
+        action_size: int --> action size (4 here)
+        gamma: float --> discount factor for return to go
+
+    """
+    def __init__(
+        self,
+        max_len: int,
+        state_size: int,
+        action_size: int,
+        gamma: float = 0.99,
+    ) -> None:
+        self.max_length = max_len
+        self.max_ep_len = 4096
+        self.state_size = state_size
+        self.action_size = action_size
+        self.gamma = gamma
+        self.device = torch.device(
+            "cuda" if torch.cuda.is_available() else "cpu"
+        )
+
+    def split_data(
+        self,
+        state_set: np.ndarray,
+        next_state_set: Optional[np.ndarray],
+        action_set: np.ndarray,
+        return_set: np.ndarray,
+    ) -> Tuple[torch.Tensor]:
+        """
+        Split the full data into subsets based on the length of the sequence (self.max_len)
+
+        Number of iterations: (state.shape[1] - self.max_length + 1)
+        in each iter, we slice a piece of the data with window size = self.max_length
+
+        Data format: stack row wise with every 20 days of data
+
+        Args:
+            state_set: np.ndarray --> full states with shape (k, d, 4)
+            next_state_set: np.ndarray --> full next states with shape (k, d, 4)
+            action_set: np.ndarray --> full actions with shape (k, d, 4)
+            return_set: np.ndarray --> full returns with shape (k, d)
+
+        Returns:
+            Tuple[torch.Tensor]: (s, a, rt, t, mask)
+            s --> stacked state with shape
+                (k * (state_set.shape[1] - self.max_length+1)), self.max_length, 4)
+            ns --> stacked next state with shape
+                (k * (next_state_set.shape[1] - self.max_length+1)), self.max_length, 4)
+            a --> stacked action with shape
+                (k * (state_set.shape[1] - self.max_length+1)), self.max_length, 4)
+            rt --> stacked return to go with shape
+                (k * (state_set.shape[1] - self.max_length+1)), self.max_length)
+            t --> stacked timesteps with shape
+                (k * (state_set.shape[1] - self.max_length+1)), self.max_length)
+                output of every t: [0, 1, ..., 19]
+            mask --> stacked mask with shape
+                (k * (state_set.shape[1] - self.max_length+1)), self.max_length)
+        """
+        s, ns, a, t, rt, mask = [], [], [], [], [], []
+
+        for start_idx in range(state_set.shape[1] - self.max_length + 1):
+            state_piece = state_set[:, start_idx: start_idx + self.max_length, :]  # (k, 20, 4)
+            seq_length = state_piece.shape[1]
+            state_norm = min_max_norm(state_piece)
+            s.append(state_norm)
+
+            if next_state_set is not None:
+                next_state_piece = next_state_set[:, start_idx: start_idx + self.max_length, :]  # (k, 20, 4)
+                seq_length = next_state_piece.shape[1]
+                next_state_norm = min_max_norm(next_state_piece)
+                ns.append(next_state_norm)
+
+            action_piece = action_set[:, start_idx: start_idx + self.max_length, :]  # (k, 20, 4)
+            a.append(action_piece)
+            timesteps = np.arange(self.max_length)
+            expand_timesteps = np.tile(timesteps, state_set.shape[0]).reshape(state_set.shape[0], timesteps.shape[0])
+            t.append(expand_timesteps)
+            returns_to_go_piece = count_return_to_go(
+                return_set[:, start_idx: start_idx + self.max_length],  # (k, 20)
+                self.gamma
+            )
+            rt.append(returns_to_go_piece)
+            mask.append(np.ones((state_set.shape[0], seq_length)))
+
+        s = torch.from_numpy(
+            np.concatenate(s, axis=0)
+        ).to(dtype=torch.float32, device=self.device)
+
+        if next_state_set is not None:
+            ns = torch.from_numpy(
+                np.concatenate(ns, axis=0)
+            ).to(dtype=torch.float32, device=self.device)
+
+        a = torch.from_numpy(
+            np.concatenate(a, axis=0)
+        ).to(dtype=torch.float32, device=self.device)
+        t = torch.from_numpy(
+            np.concatenate(t, axis=0)
+        ).to(dtype=torch.float32, device=self.device)
+        rtg = torch.from_numpy(
+            np.concatenate(rt, axis=0)
+        ).to(dtype=torch.float32, device=self.device)
+        mask = torch.from_numpy(
+            np.concatenate(mask, axis=0)
+        ).to(dtype=torch.float32, device=self.device)
+
+        return s, ns, a, t, rtg, mask
---
+++ utils.py
@@ -0,0 +1,183 @@
+from typing import Any, Dict, List, Union
+import yaml
+import numpy as np
+import torch
+
+
+def load_config(cfg_path: str) -> Dict[str, Any]:
+    """
+    Load configuration file
+    """
+    with open(cfg_path, 'r', encoding="utf-8") as cfg:
+        config = yaml.safe_load(cfg)
+
+    return config
+
+
+def min_max_norm(x: np.ndarray) -> np.ndarray:
+    """
+    min-max normalization for 3-dim matrix
+
+    keep the shape of min, max as (n, 20, 1)
+    """
+    normed_array = np.zeros_like(x)
+
+    for strat in range(x.shape[0]):
+        min_val = np.min(x[strat], axis=1, keepdims=True)
+        max_val = np.max(x[strat], axis=1, keepdims=True)
+        normed_array[strat] = (x[strat] - min_val) / (max_val - min_val)
+
+    return normed_array
+
+
+def count_return_to_go(returns: np.ndarray, gamma: float) -> np.ndarray:
+    """
+    Compute the return to go (sum of future rewards)
+    """
+    rtg = np.zeros_like(returns)
+
+    for strat in range(returns.shape[0]):
+        rtg[strat][-1] = returns[strat][-1]
+
+        for t in reversed(range(returns[strat].shape[0] - 1)):
+            rtg[strat][t] = returns[strat][t] + gamma * rtg[strat][t + 1]
+
+    return rtg
+
+
+def compute_dr(
+    close_today,
+    close_tomorrow: float,
+    action_today: np.ndarray,
+    have_position: bool,
+    prev_act: int | None,
+    trans_cost: float = 0.001,
+    portfolio_value: float = 1000000
+) -> float:
+    """
+    compute daily return
+
+    input:
+    - n: amount of strategies
+
+    - close_today: float
+    - close_tomorrow: float
+    - action_today: (4,) vector [buy, sell, buytocover, short]
+    """
+    action = np.argmax(action_today.cpu().numpy())
+
+    if isinstance(close_today, torch.Tensor):
+        close_today = close_today.cpu().numpy()
+
+    if close_today == 0.0:
+        close_today += 1e-4
+
+    share_today = np.floor(portfolio_value / close_today)
+    cash_today = portfolio_value - share_today * close_today
+    trans_cost_today = share_today * close_today * trans_cost
+    portfolio_value_tommorow = share_today * close_tomorrow + cash_today - trans_cost_today
+    long_dr = (portfolio_value_tommorow - portfolio_value) / portfolio_value
+    short_dr = (-portfolio_value_tommorow + portfolio_value) / portfolio_value
+
+    if have_position is False:
+        if action == 0:
+            have_position = True
+            dr = long_dr
+
+        if action == 3:
+            have_position = True
+            dr = short_dr
+
+        else:
+            dr = 0
+
+        prev_act = action
+
+        return dr, have_position, prev_act
+
+    if have_position is True:
+        if prev_act == 0:
+
+            if action in [0, 2]:
+                have_position = True
+                prev_act = 0
+                dr = long_dr
+
+            if action in [1, 3]:
+                have_position = False
+                prev_act = action
+                dr = 0
+
+        elif prev_act == 3:
+
+            if action in [1, 3]:
+                have_position = True
+                prev_act = 3
+                dr = short_dr
+
+            if action in [0, 2]:
+                have_position = False
+                prev_act = action
+                dr = 0
+
+        return dr, have_position, prev_act
+
+
+def get_start_year_idx(
+    year_list: List[int],
+    timesteps: np.ndarray
+) -> Dict[int, int]:
+    """
+    Get the start index of each year
+    """
+    year_start_idx = {}
+
+    for y in year_list:
+        for idx, date in enumerate(timesteps[-1]):
+            if str(y) in date:
+                year_start_idx[y] = idx
+                break
+
+    return year_start_idx
+
+
+def get_slicev2(
+    data: List[Union[np.ndarray, torch.Tensor]],
+    num_strats: int,
+    start: int = 0,
+    end: int | None = None
+) -> List[Union[np.ndarray, torch.Tensor]]:
+    """
+    slice the data into length of training
+
+    The training length -> first day until the specifc year (end - max_len)
+    """
+    output = []
+
+    for d in data:
+        if end is not None:
+            output.append(d[(start * num_strats): (end * num_strats), :])
+        else:
+            output.append(d[(start * num_strats):, :])
+
+    return output
+
+
+def get_test_slicev2(
+    data: List[Union[np.ndarray, torch.Tensor]],
+    num_strats: int,
+    train_len: int,
+    max_len: int,
+    year_start_idx: int,
+    test_year: int,
+    year_list: List[str],
+) -> List[Union[np.ndarray, torch.Tensor]]:
+    """
+    slice the data into length of testing
+
+    The testing length -> last 19 days of training data until the specific year end
+    """
+    start = train_len - (max_len - 1)  # start from last 19 days of training data
+    end = None if test_year == year_list[-1] else year_start_idx[test_year + 1]
+
+    return get_slicev2(data, num_strats, start, end)
